8dd7fd0f5e295df78cbc4733805c2b0827f775d4
QuiterHe <hezhang1001@foxmail.com>
2017-02-17 15:03:18 +0800

    新加算法模块--quickSort

---
+++ DS/QuickSort.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: he
+ * Date: 17-2-17
+ * Time: 下午2:54
+ */
+
+
+function quickSort(array &$arr, $low, $high) {
+    if( $low < $high ) {
+        $pi = partition($arr, $low, $high);
+
+        quickSort($arr, $low, $pi-1);
+        quickSort($arr, $pi+1, $high);
+    }
+}
+
+function partition(array &$arr, $low, $high) {
+    $pivot = $arr[$high];
+
+    $i = $low - 1;
+    for($j=$low; $j<$high; $j++) {
+        if( $arr[$j] <= $pivot ) {
+            $i++;
+            swapArr($arr, $i, $j);
+        }
+    }
+    swapArr($arr, $i+1, $high);
+    return $i + 1;
+
+}
+
+function swapArr(array &$arr, $a, $b) {
+    $c = $arr[$a];
+    $arr[$a] = $arr[$b];
+    $arr[$b] = $c;
+}
+
+
+$arr = [10, 80, 30, 90, 40, 50, 70];
+quickSort($arr, 0, 6);
+var_dump($arr);
+
